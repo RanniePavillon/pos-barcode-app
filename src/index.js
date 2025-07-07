99b904439d9544f5e4409b74e77711426e866e49
ranniepavillon <ranniepavillon@gmail.com>
2025-07-07 15:34:13 +0800

    hello world

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 // Libraries
-import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Index as IndexBusiness } from './__business'
-import { Index as IndexPersonal } from './__personal'
 
-const loc = window.location.pathname
-const subdir = loc.split('/')[1]
-let Core
-
-if (subdir === 'psl') {
-	Core = IndexPersonal
-} else if(subdir === 'biz'){
-	Core = IndexBusiness
-}
-else{
-	window.location.href = "/biz"
-	// Core = IndexBusiness
-	// Core = IndexPersonal
-}
+let Core = IndexBusiness
 
 ReactDOM.render(
 <Core/>, document.getElementById('root'));
